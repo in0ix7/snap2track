2185286018191f57d5f2fe3fb602443f82e4763b
Ilia Nilov <in0ix7@gmail.com>
2026-04-14 13:47:22 +0300

    A00001 - Initial project from Spring Initializr

---
+++ src/main/java/com/fitcoach/snap2track/Snap2trackApplication.java
@@ -0,0 +1,13 @@
+package com.fitcoach.snap2track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Snap2trackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Snap2trackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitcoach/snap2track/Snap2trackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitcoach.snap2track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Snap2trackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
